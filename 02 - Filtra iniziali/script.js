fcea86361994b66b61dd18a3191b100c02298f29
l-martinetti <luca.martinetti@outlook.it>
2024-11-27 18:16:35 +0100

    feat: add ex-02 complet

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function onlyA (array, letter) {
+    let newArray = [];
+    
+    for (let i = 0; i < names.length; i++){
+        let word = names[i]; 
+
+        if (word.substring(0, 1).toUpperCase() === "A"){
+            newArray.push(word);              
+        } 
+    }
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(onlyA(names));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
